8af156f6f5a7d7140828042d3c6779c81ac80a3b
Paul Trebilcox-Ruiz <paultr@Pauls-MacBook-Pro.local>
2016-04-23 12:55:39 -0600

    inital project

---
+++ app/src/main/java/com/ptrprograms/androidtvplayground/PlayerActivity.java
@@ -0,0 +1,44 @@
+package com.ptrprograms.androidtvplayground;
+
+import android.app.Activity;
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.VideoView;
+
+import java.io.File;
+
+/**
+ * Created by Paul on 10/10/15.
+ */
+public class PlayerActivity extends Activity implements PlayerControlsFragment.PlayerControlsListener {
+    private VideoView mVideoView;
+    private Video mVideo;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_player);
+
+        initViews();
+        initVideoPlayer();
+    }
+
+    private void initViews() {
+        mVideoView = (VideoView) findViewById( R.id.video_view );
+    }
+
+    private void initVideoPlayer() {
+        mVideo = (Video) getIntent().getSerializableExtra( VideoDetailsFragment.EXTRA_VIDEO );
+        mVideoView.setVideoPath( mVideo.getVideoUrl() );
+    }
+
+    @Override
+    public void play() {
+        mVideoView.start();
+    }
+
+    @Override
+    public void pause() {
+        mVideoView.pause();
+    }
+}
